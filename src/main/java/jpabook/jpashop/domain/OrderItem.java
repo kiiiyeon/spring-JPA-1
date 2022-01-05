8afe8c06710c0297aeab110ab587b0c76403de40
kiiiyeon <rusl0325@sookmyung.ac.kr>
2022-01-05 18:25:33 +0900

    [0105] 6강 완료

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -27,4 +29,33 @@ public class OrderItem {
     private int orderPrice;
 
     private int count;
+
+    //@NoArgsConstructor(access = AccessLevel.PROTECTED) OrderItem 클래스 위에 어노테이션 붙여주면 아래 코드 없어도 됨, 코드에 제약을 두면서 작성하자!
+    protected OrderItem() {
+
+    }
+
+    //생성 메서드
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.remoeStock(count);
+        return orderItem;
+    }
+
+    //비즈니스 로직
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    //조회 로직
+    /**
+     * 주문 상품 가격 전체 조회
+     */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
